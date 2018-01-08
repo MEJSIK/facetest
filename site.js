bac9f59ddde3023f1b09885d80c039eb98bd468f
MEJS <bartekwalasik@gmail.com>
2018-01-08 14:06:21 +0100

    Add files via upload

---
+++ site.js
@@ -0,0 +1,28 @@
+var video = document.getElementById('video');
+var canvas = document.getElementById('motion');
+var score = document.getElementById('score');
+var bgvid = document.getElementById('bgvid');
+
+function initSuccess() {
+    DiffCamEngine.start();
+}
+
+function initError() {
+    alert('Something went wrong.');
+}
+
+function capture(payload) {
+
+    if (payload.score > 40) {
+        bgvid.play();
+    } else bgvid.pause();
+    score.textContent = payload.score;
+}
+
+DiffCamEngine.init({
+    video: video,
+    motionCanvas: canvas,
+    initSuccessCallback: initSuccess,
+    initErrorCallback: initError,
+    captureCallback: capture
+});
