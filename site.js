9f7aeb0c56ae27abe37ce6dd42c459f34ff38bc0
MEJS <bartekwalasik@gmail.com>
2018-01-08 16:22:05 +0100

    Update site.js

--- site.js
+++ site.js
@@ -13,7 +13,7 @@ function initError() {
 
 function capture(payload) {
 
-    if (payload.score > 150) {
+    if (payload.score > 250) {
         bgvid.play();
     } else bgvid.pause();
     score.textContent = payload.score;
